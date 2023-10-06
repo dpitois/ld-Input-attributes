3f52a6de486dc6de39285a3480cda4f89306aca3
dazbo <7702006+dpitois@users.noreply.github.com>
2023-10-06 13:10:43 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-// import { LdInput, LdLabel } from "@emdgroup-liquid/liquid/dist/react-define-excluded"
-// import { LdInput, LdLabel } from "@emdgroup-liquid/liquid/dist/react-define-excluded"
-import { LdInput, LdLabel, LdTypo } from "@emdgroup-liquid/liquid/dist/react";
+import {
+  LdInput,
+  LdLabel,
+  LdTypo,
+} from "@emdgroup-liquid/liquid/dist/react-define-excluded";
+// import { LdInput, LdLabel, LdTypo } from "@emdgroup-liquid/liquid/dist/react";
 
 function App() {
   return (
     <>
       <LdTypo>Still editable</LdTypo>
       <div>
-      <LdLabel position="right">
+        <LdLabel position="right">
           <LdInput disabled={true} /> disabled with boolean
         </LdLabel>
       </div>
@@ -18,13 +21,15 @@ function App() {
       </div>
       <div>
         <LdLabel position="right">
-          <LdInput disabled={true} readonly={true} /> disabled with boolean + readonly with boolean
+          <LdInput disabled={true} readonly={true} /> disabled with boolean +
+          readonly with boolean
         </LdLabel>
       </div>
       <LdTypo>Not editable (as expected)</LdTypo>
       <div>
         <LdLabel position="right">
-          <LdInput disabled={true} readonly="true" /> disabled with boolean + readonly as string
+          <LdInput disabled={true} readonly="true" /> disabled with boolean +
+          readonly as string
         </LdLabel>
       </div>
       <div>
