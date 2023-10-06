e6c94786a70c2282142c598c471205644f6ac314
CodeSandbox <support@codesandbox.io>
2023-10-06 09:07:48 +0000

    Sandbox snapshot commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import {
+  LdInput,
+  LdLabel,
+} from "@emdgroup-liquid/liquid/dist/react-define-excluded";
+
+function App() {
+  return (
+    <>
+      <div>
+        <LdLabel>
+          <LdInput disabled={true} /> disabled
+        </LdLabel>
+      </div>
+      <div>
+        <LdLabel>
+          <LdInput disabled={true} readonly /> disabled + readonly
+        </LdLabel>
+      </div>
+      <div>
+        <LdLabel>
+          <LdInput disabled={true} readonly={true} /> disabled + readonly
+        </LdLabel>
+      </div>
+      <div>
+        <LdLabel>
+          <LdInput disabled={true} readonly={"true"} /> disabled +
+          readonly.string
+        </LdLabel>
+      </div>
+      <div>
+        <LdLabel>
+          <LdInput readonly={"true"} /> disabled + readonly.string
+        </LdLabel>
+      </div>
+      <div>
+        <LdLabel>
+          <LdInput disabled={"true"} /> disabled.string
+        </LdLabel>
+      </div>
+    </>
+  );
+}
+
+export default App;
